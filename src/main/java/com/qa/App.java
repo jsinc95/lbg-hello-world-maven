2b13ef91820dc969ab2c8371b1cb2eef8a2c1dc3
Peter <peter@qa.com>
2024-07-02 14:55:15 +0100

    added new line

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -21,7 +21,7 @@ public class App
     }
 
     public static String sayGoodbye(){
-        return "Goodbye World!";
+        return "Hello happy  World!";
     }
 
     public static String sayHelloToSomeone(String name){
